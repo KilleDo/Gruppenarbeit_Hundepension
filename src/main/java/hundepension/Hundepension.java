df20dbae31f9371e0f0bd570305d9e486a7f9d47
KilleDo <k.domhoever@gmx.de>
2024-12-06 10:42:33 +0100

    Anfang GUI (alter fehlt noch)

---
+++ src/main/java/hundepension/Hundepension.java
@@ -0,0 +1,15 @@
+package hundepension;
+
+import javax.swing.*;
+
+public class Hundepension {
+    private JLabel registration;
+    private JTextField nameHundtextfield;
+    private JComboBox rassecombobox;
+    private JTextField rassetextfield;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JComboBox comboBox4;
+    private JTextField textField2;
+}
