c748e0a9b20e83dedbb08117a528516d0a367900
KilleDo <k.domhoever@gmx.de>
2024-12-06 11:38:15 +0100

    Rasse (andere:) funktioniert jetzt

--- src/main/java/hundepension/Hundepension.java
+++ src/main/java/hundepension/Hundepension.java
@@ -1,15 +1,55 @@
 package hundepension;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Hundepension {
+public class Hundepension extends JFrame{
     private JLabel registration;
     private JTextField nameHundtextfield;
     private JComboBox rassecombobox;
     private JTextField rassetextfield;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
-    private JComboBox comboBox3;
-    private JComboBox comboBox4;
-    private JTextField textField2;
+    private JComboBox geschlechtcombobox;
+    private JComboBox kastriertcombobox;
+    private JComboBox groeßecombobox;
+    private JComboBox problemecombobox;
+    private JTextField problemetextfield;
+    private JTextField altertextfield;
+    private JPanel hundregistrierpanel;
+    private JLabel rasselabel;
+    private JLabel groeßelabel;
+    private JLabel alterlabel;
+    private JLabel geschlechtlabel;
+    private JLabel kastriertlabel;
+    private JLabel problemelabel;
+    private JLabel namelabel;
+    boolean problemsichtbar = false;
+
+    public Hundepension(){
+        setTitle("Hundepension ");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(hundregistrierpanel);
+        setSize(1500,400); //größe des Fensters
+        setVisible(true); //Fenster sichtbar machen
+
+        rassetextfield.setVisible(false);
+        problemetextfield.setVisible(false);
+
+
+        rassecombobox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String rasse = rassecombobox.getSelectedItem().toString();
+
+                if (rasse.equals("andere:")){
+                    rassetextfield.setVisible(true);
+                }else{
+                    rassetextfield.setVisible(false);
+                }}
+        });
+    }
+
+    public static void main(String[] args) {
+        new Hundepension();
+    }
 }
